f5689e9b6dd2803bba96941139d03b391990dfca
Muhammad Rovino Sanjaya <rovino.sanjaya@primavista-solusi.com>
2024-10-03 15:25:23 +0700

    init

---
+++ app/Http/Controllers/OldPasswordController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OldPassword;
+use App\Http\Requests\StoreOldPasswordRequest;
+use App\Http\Requests\UpdateOldPasswordRequest;
+
+class OldPasswordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOldPasswordRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OldPassword $oldPassword)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OldPassword $oldPassword)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOldPasswordRequest $request, OldPassword $oldPassword)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OldPassword $oldPassword)
+    {
+        //
+    }
+}

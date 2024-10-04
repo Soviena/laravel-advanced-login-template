9b7c92f2542d4b80dee41356bdbc423595d204c2
Muhammad Rovino Sanjaya <rovino.sanjaya@primavista-solusi.com>
2024-10-04 14:45:09 +0700

    add username password

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -87,13 +87,27 @@
                   </div>
                 </div>
                 <div class="mb-6">
+                  <label class="form-label" for="basic-icon-default-fullname">Username</label>
+                  <div class="input-group input-group-merge">
+                    <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
+                    <input type="text" class="form-control" id="basic-icon-default-fullname" placeholder="John" aria-label="John Doe" aria-describedby="basic-icon-default-fullname2">
+                  </div>
+                </div>
+                <div class="mb-6">
                   <label class="form-label" for="basic-icon-default-email">Email</label>
                   <div class="input-group input-group-merge">
                     <span class="input-group-text"><i class="bx bx-envelope"></i></span>
-                    <input type="text" id="basic-icon-default-email" class="form-control" placeholder="john.doe" aria-label="john.doe" aria-describedby="basic-icon-default-email2">
+                    <input type="email" id="basic-icon-default-email" class="form-control" placeholder="john.doe" aria-label="john.doe" aria-describedby="basic-icon-default-email2">
                     <span id="basic-icon-default-email2" class="input-group-text">@example.com</span>
                   </div>
                   <div class="form-text"> You can use letters, numbers &amp; periods </div>
+                </div>
+                <div class="mb-6">
+                  <label class="form-label" for="basic-icon-default-fullname">Password</label>
+                  <div class="input-group input-group-merge">
+                    <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
+                    <input type="password" class="form-control" id="basic-icon-default-fullname" placeholder="John" aria-label="John Doe" aria-describedby="basic-icon-default-fullname2">
+                  </div>
                 </div>
                 <div class="mb-6">
                   <label class="form-label" for="basic-icon-default-phone">Phone No</label>
